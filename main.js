0eb93ece15f2a36b2917561bdc7c1732e1c89c91
Craig McKeachie <craigdmckeachie@gmail.com>
2024-08-05 16:19:35 -0400

    fruit list example

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-function HelloWorld() {
-  return <div className="container">Hello Component</div>;
-}
-
-function App() {
+function FruitListCard({ fruit }) {
   return (
-    <>
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
-    </>
+    <div style={{ border: "1px solid black" }}>
+      <strong>{fruit.name}</strong>
+      <p> ID: {fruit.id}</p>
+      <p>Color: {fruit.color}</p>
+    </div>
   );
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.createRoot(rootElement).render(<App />);
+function FruitList(props) {
+  const fruitListItems = props.fruits.map((fruit) => (
+    <FruitListCard key={fruit.id} fruit={fruit} />
+  ));
+  return <div>{fruitListItems}</div>;
+}
+
+const data = [
+  { id: 1, name: "apple", color: "red" },
+  { id: 2, name: "orange", color: "orange" },
+  { id: 3, name: "blueberry", color: "blue" },
+  { id: 4, name: "banana", color: "yellow" },
+  { id: 5, name: "kiwi", color: "brown" },
+];
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <FruitList fruits={data} />
+);
