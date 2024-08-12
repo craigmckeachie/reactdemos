b08ff0f076873601045125655e1098e47a9364b2
Craig McKeachie <craigdmckeachie@gmail.com>
2024-08-12 15:51:33 -0400

    update

--- main.js
+++ main.js
@@ -1,91 +1,73 @@
-//main.js
-const { useState, useEffect } = React;
+const {
+  BrowserRouter: Router,
+  Route,
+  Routes,
+  Link,
+  NavLink,
+  Navigate,
+  useParams,
+  useLocation,
+  useNavigation,
+} = ReactRouterDOM;
 
-const BASE_URL = "http://localhost:9000";
-
-function translateStatusToErrorMessage(status) {
-  switch (status) {
-    case 401:
-      return "Please sign in again.";
-    case 403:
-      return "You do not have permission to view the data requested.";
-    default:
-      return "There was an error saving or retrieving data.";
-  }
+function Home() {
+  return <h2>Home</h2>;
 }
 
-async function checkStatus(response) {
-  if (response.ok) return response;
-
-  const httpError = {
-    status: response.status,
-    statusText: response.statusText,
-    url: response.url,
-    body: await response.text(),
-  };
-  console.log(`http error status: ${JSON.stringify(httpError, null, 1)}`);
-
-  let errorMessage = translateStatusToErrorMessage(httpError.status);
-  throw new Error(errorMessage);
+function About() {
+  return <h2>About</h2>;
 }
 
-function parseJSON(response) {
-  return response.json();
+function Contact() {
+  return <h2>Contact</h2>;
 }
 
-function delay(ms) {
-  return function (x) {
-    return new Promise((resolve) => setTimeout(() => resolve(x), ms));
-  };
-}
-
-const url = `${BASE_URL}/songs`;
-const songAPI = {
-  list() {
-    return fetch(url).then(checkStatus).then(parseJSON);
-  },
-};
-
-function SongList() {
-  const [busy, setBusy] = useState(false);
-  const [songs, setSongs] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(undefined);
-
-  async function loadSongs() {
-    try {
-      setBusy(true);
-      let data = await songAPI.list();
-      setSongs(data);
-    } catch (error) {
-      setErrorMessage(error.message);
-    } finally {
-      setBusy(false);
-    }
-  }
-
-  useEffect(function () {
-    loadSongs();
-  }, []);
-
+function NotFound() {
   return (
-    <div className="list mt-2">
-      {busy && <p>Loading...</p>}
-      {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-      {songs?.map((song) => (
-        <div className="card p-4" key={song.songID}>
-          <strong>{song.title}</strong>
-          <div>{song.artist}</div>
-        </div>
-      ))}
-    </div>
+    <>
+      <h2>Uh oh.</h2>
+      <p>
+        The page you requested could not be found. Is there any chance you were
+        looking for one of these?
+      </p>
+    </>
   );
 }
 
 function App() {
   return (
-    <div className="container">
-      <SongList />
-    </div>
+    <Router>
+      <div>
+        <nav className="container mt-4">
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
