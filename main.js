68fb5dc1e257acedf59275af2828607b7d5be7b3
Craig McKeachie <craigdmckeachie@gmail.com>
2024-08-13 09:42:43 -0400

    add song link

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { useState, useEffect } = React;
+
 const {
   BrowserRouter: Router,
   Route,
@@ -10,12 +12,101 @@ const {
   useNavigation,
 } = ReactRouterDOM;
 
+const BASE_URL = "http://localhost:9000";
+
+//fetch Utility functions
+
+function translateStatusToErrorMessage(status) {
+  switch (status) {
+    case 401:
+      return "Please sign in again.";
+    case 403:
+      return "You do not have permission to view the data requested.";
+    default:
+      return "There was an error saving or retrieving data.";
+  }
+}
+
+async function checkStatus(response) {
+  if (response.ok) return response;
+
+  const httpError = {
+    status: response.status,
+    statusText: response.statusText,
+    url: response.url,
+    body: await response.text(),
+  };
+  console.log(`http error status: ${JSON.stringify(httpError, null, 1)}`);
+
+  let errorMessage = translateStatusToErrorMessage(httpError.status);
+  throw new Error(errorMessage);
+}
+
+function parseJSON(response) {
+  return response.json();
+}
+
+function delay(ms) {
+  return function (x) {
+    return new Promise((resolve) => setTimeout(() => resolve(x), ms));
+  };
+}
+
+// API object, communicates with the API or the backend
+const url = `${BASE_URL}/songs`;
+
+const songAPI = {
+  list() {
+    return fetch(url).then(checkStatus).then(parseJSON);
+  },
+};
+
+//Components UI
 function HomePage() {
   return <h2>Home</h2>;
 }
 
 function SongsPage() {
-  return <h2>Songs</h2>;
+  return (
+    <div>
+      <header className="d-flex justify-content-between mb-4">
+        <h2>Songs</h2>
+        <Link className="btn btn-outline-secondary" to="/songs/create">+ Add Song</Link>
+      </header>
+      <hr />
+      <SongList />
+    </div>
+  );
+}
+
+function SongList() {
+  const [songs, setSongs] = useState([]);
+  const [busy, setBusy] = useState(false);
+
+  async function loadSongs() {
+    setBusy(true);
+    let data = await songAPI.list();
+    setSongs(data);
+    setBusy(false);
+  }
+
+  useEffect(() => {
+    loadSongs();
+  }, []);
+
+  return (
+    <div className="list">
+      {busy && <div>Loading...</div>}
+
+      {songs.map((song) => (
+        <div className="card p-4" key={song.songID}>
+          <strong>{song.title}</strong>
+          <div>{song.artist}</div>
+          <small>{song.year}</small>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 function ArtistsPage() {
